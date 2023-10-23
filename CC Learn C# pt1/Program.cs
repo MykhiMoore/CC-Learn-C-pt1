8368379ccf3503ee3e204172f80865d7d7616636
MykhiMoore <moore_mykhi@student.mahoningctc.com>
2023-10-23 12:28:27 -0400

    Add project files.

---
+++ CC Learn C# pt1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CC_Learn_C__pt1
+
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello Mykhi!");
+            Console.WriteLine("What is your age");
+            string input = Console.ReadLine();
+            Console.WriteLine($"you are {input} years old");
+            //Comments
+        }
+    }
+}
+ 
